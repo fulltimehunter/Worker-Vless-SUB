d4f2346d8d310745bed14683e3c77913e73e5167
fulltimehunter <116131922+fulltimehunter@users.noreply.github.com>
2024-05-10 16:43:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -56,10 +56,10 @@ let addresses = [
 ];
 
 // 设置优选地址api接口
-let addressesapi = [
+//let addressesapi = [
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
-];
+//];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
 let addressesnotls = [
@@ -114,9 +114,9 @@ let addressesnotls = [
 ];
 
 // 设置优选noTLS地址api接口
-let addressesnotlsapi = [
+//let addressesnotlsapi = [
 	//'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
-];
+//];
 
 let DLS = 8;//速度下限
 let addressescsv = [
